5df25b1d22bc8fcc2fc9c80e8ba7a8f97fa0b065
Matthew Cavallo <macavall@microsoft.com>
2024-04-21 16:14:52 -0500

    checkpoint

--- Program.cs
+++ Program.cs
@@ -9,7 +9,6 @@ namespace DependencyInjectionAPI
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
-
             builder.Services.AddControllers();
 
             builder.Services.AddTransient<IGreetingTransient, GreetingServiceTransient>();
@@ -23,7 +22,6 @@ namespace DependencyInjectionAPI
             app.UseHttpsRedirection();
 
             app.UseAuthorization();
-
 
             app.MapControllers();
 
